f361784eb0e64a5b22da26993ca90801a3d08d11
Paul-Colin <paul.colin@ig2i.centralelille;fr>
2021-02-16 08:43:21 +0100

    modification de biblio

--- biblio.h
+++ biblio.h
@@ -25,6 +25,7 @@ int  afficherBibliotheque(const T_Bibliotheque  *ptrB);
 int rechercherTitre(T_Bibliotheque *ptrB,char *rechercheT);
 int rechercherAuteur(T_Bibliotheque *ptrB,char *rechercheA);
 int supprimer(T_Bibliotheque *ptrB, char *rechercheT,char *rechercheA) ;
-
+void sauvegarde(T_Bibliotheque *ptrB);
+void chargement(T_Bibliotheque *ptrB);
 
 #endif
--- tpBiblio.c
+++ tpBiblio.c
@@ -48,6 +48,7 @@ char *rechercheLivre ;//pointeur titre
 char *rechercheAuteur;//pointeur auteur
 T_Bibliotheque B; 
 init( &B );
+chargement(&B);
 
 do
 {
@@ -99,7 +100,7 @@ switch(chx)
 	}
 
 }while(chx!=0);
-
+sauvegarde(&B);
 
 
 
